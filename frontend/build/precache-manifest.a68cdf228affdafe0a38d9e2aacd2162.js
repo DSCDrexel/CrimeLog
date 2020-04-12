f4440b44dfc77f14693fc7c6b63fc64c93016669
Shashwat Singh <shashanoid@gmail.com>
2020-04-11 21:34:06 -0400

    Add build

---
+++ frontend/build/precache-manifest.a68cdf228affdafe0a38d9e2aacd2162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baf027764da8fff2ffe9dd97a8069a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a39bb494b6dbc8f7b0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e617d2b21e074e07126",
+    "url": "/static/js/2.b3e91742.chunk.js"
+  },
+  {
+    "revision": "bdbc5366cf7566544505f28b7e1493d8",
+    "url": "/static/js/2.b3e91742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a39bb494b6dbc8f7b0",
+    "url": "/static/js/main.01e19fba.chunk.js"
+  },
+  {
+    "revision": "6f39cdd2f37af2f86a49",
+    "url": "/static/js/runtime-main.1505a909.js"
+  }
+]);
